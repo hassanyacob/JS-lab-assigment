d45996fb7ba8e40237488665158c79af9092a647
hassanyacob <72223296+hassanyacob@users.noreply.github.com>
2020-12-02 19:15:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myObj.name;
+  }
+};
+xmlhttp.open("GET", "json_demo.txt", true);
+xmlhttp.send();
+
+
 //Receiving Data
 const hwobj = JSON.parse(hw);
 const obj = JSON.parse('[
