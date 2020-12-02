07016ec5c18ffb048c8108e82b5f6c2e5b59ea99
hassanyacob <72223296+hassanyacob@users.noreply.github.com>
2020-12-02 23:14:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ fetch("hw.json")
 .then(data => {
      console.log(data)
      const myObj = JSON.parse(this.responseText)
-     document.quuerySelector(#demo).innerTxet =myObj.topping
+     document.quuerySelector(#demo).innerText =myObj.topping
 })
