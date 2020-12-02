ad473e1b2517b2c78957198a4fb205e183d7c129
hassanyacob <72223296+hassanyacob@users.noreply.github.com>
2020-12-02 22:59:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-fetch("hw.json");
-.then(response => response.json());
+fetch("hw.json")
+.then(response => response.json())
 .then(data => {
-     console.log(data);
-});
+     console.log(data)
+})
